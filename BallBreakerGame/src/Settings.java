45ccf59238e52518c03a3e78339885e6f9b83c05
CoreeJacob <coreemcgrath@hotmail.com>
2022-10-01 00:58:54 +1300

    Initial Commit -
    Initial commit for Brick Breaker game for Assignment 3 -
    
    Spent time familiarising myself with the overall structure of the
    program. Noticed a number of similarities with previous programs
    
    Have added a few comments onto the SETTINGS class - these are for
    description only and offer no functional purpose
    
    Next Task:
    
    Is to set the BALL and PADDLE classes up, that includes setting their
    physical properties, initial start positions etc.
    
    Will also work on the SPRITE class which houses a number of shared
    properties for both the BALL and PADDLE classes.
    
    All necessary variables are being housed in the SETTINGS class - are set
    up as FINAL so will not be edited any further.

---
+++ BallBreakerGame/src/Settings.java
@@ -0,0 +1,30 @@
+
+public class Settings {
+	public static final String WINDOW_NAME = "Breakout";	//name of game
+	
+	
+	public static final int WINDOW_WIDTH = 400;				//size of window
+	public static final int WINDOW_HEIGHT = 500;
+	
+	
+	public static final int TOTAL_BRICKS = 20;
+	public static final int BRICK_VERT_PADDING = 50;
+	public static final int BRICK_HEIGHT = 30;				//brick variables
+	public static final int BRICK_HORI_PADDING = 50;
+	public static final int BRICK_WIDTH = 70;
+	
+
+	public static final int INITIAL_PADDLE_X = 100;
+	public static final int INITIAL_PADDLE_Y = 370;			//paddle variables incl initial start position
+	public static final int PADDLE_WIDTH = 100;
+	public static final int PADDLE_HEIGHT = 15;
+	
+	public static final int INITIAL_BALL_X = 150;
+	public static final int INITIAL_BALL_Y = 350;
+	public static final int BALL_WIDTH = 10;				//ball variables incl initial start position
+	public static final int BALL_HEIGHT = 10;
+	
+	public static final int MESSAGE_POSITION = 270;
+	public static final int LIVES_POSITION_X = 5;			//variables to show how many lives are left in the game
+	public static final int LIVES_POSITION_Y = 15;
+}
---
+++ BallBreakerGame/src/Sprite.java
@@ -0,0 +1,37 @@
+import java.awt.Rectangle;
+
+public class Sprite {
+	
+	protected int x,y,width,height;
+	
+	// Note: This should only set a single value, they can be done in a single statement
+	public void setX(int x) {
+	}
+	public void setY(int y) { 
+		// TODO
+	}
+	public void setWidth(int width) { 
+		// TODO
+	}
+	public void setHeight(int height) { 
+		// TODO
+	}
+	
+	// Note: Change the "0" to the correct variable
+	public int getX() { 
+		return 0;	// TODO: Return correct value
+	}
+	public int getY() { 
+		return 0;	// TODO: Return correct value
+	}
+	public int getWidth() { 
+		return 0;	// TODO: Return correct value
+	}
+	public int getHeight() { 
+		return 0;	// TODO: Return correct value
+	}
+	
+	Rectangle getRectangle() {
+		return new Rectangle(x, y, width, height);
+	}
+}
